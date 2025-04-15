fad3aa4bea5b4e5ff9622eee0bb474d8966ca4b0
Aiden Baumann <baumannai@rsd407.org>
2025-04-15 10:19:38 -0700

    start a ship

---
+++ MyGame/Ship.cs
@@ -0,0 +1,28 @@
+﻿using GameEngine;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+namespace MyGame
+{
+    public class Ship : GameObject
+    {
+        private readonly Sprite _sprite = new Sprite();
+        // Creates our ship
+
+        public Ship()
+        {
+            _sprite.Texture = Game.GetTexture("Resources/ship.png");
+            _sprite.Position = new Vector2f(100, 100);
+        }
+        // Draws our ship
+        public override void Draw()
+        {
+            Game.RenderWindow.Draw(_sprite);
+        }
+        // Updates our ship every frame
+        public override void Update(Time elapsed)
+        {
+            // Do nothing just yet
+        }
+    }
+}
